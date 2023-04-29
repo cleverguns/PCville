099ebc2aa127ce6764a7bd80944b2d0e4cf53bb7
Carlo Gimps <41587193+cleverguns@users.noreply.github.com>
2023-04-29 08:44:32 +0800

    Update submit_comment.php

--- submit_comment.php
+++ submit_comment.php
@@ -1,38 +1,48 @@
 <?php
-// Connect to the database
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "cn_ecommerce";
+// Start the session
+session_start();
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    echo 'You must be logged in to submit a comment.';
+    exit;
 }
 
-// Get form data
-$user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-$product_id = mysqli_real_escape_string($conn, $_POST['product_id']);
-$comment = mysqli_real_escape_string($conn, $_POST['comment']);
-$image = "";
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Connect to the database
+    $conn = mysqli_connect('localhost', 'root', '', 'ecommerce');
 
-// Handle image upload (if provided)
-if ($_FILES['image']['error'] == UPLOAD_ERR_OK) {
-  $target_dir = "uploads/";
-  $target_file = $target_dir . basename($_FILES['image']['name']);
-  move_uploaded_file($_FILES['image']['tmp_name'], $target_file);
-  $image = $target_file;
+    if (!$conn) {
+        echo 'Error connecting to the database.';
+        exit;
+    }
+
+    // Get the user id and product id from the form data
+    $user_id = $_POST['user_id'];
+    $product_id = $_POST['product_id'];
+    $comment = $_POST['comment'];
+    
+    // Upload the image file
+    $image_file = $_FILES['image_file'];
+    $upload_dir = 'uploads/';
+    $target_file = $upload_dir . basename($image_file['name']);
+    $upload_success = move_uploaded_file($image_file['tmp_name'], $target_file);
+    
+    if (!$upload_success) {
+        echo 'Error uploading image file.';
+        exit;
+    }
+    
+    // Insert the comment data into the comments table
+    $insert_query = "INSERT INTO comments (user_id, product_id, comment, image) VALUES ('$user_id', '$product_id', '$comment', '$target_file')";
+    $insert_result = mysqli_query($conn, $insert_query);
+    
+    if (!$insert_result) {
+        echo 'Error inserting comment data into database.';
+        exit;
+    }
+    
+    // Redirect the user back to the detail page
+    header("Location: detail.php?id=$product_id");
 }
-
-// Insert comment into database
-$sql = "INSERT INTO comments (user_id, product_id, comment, image) VALUES ('$user_id', '$product_id', '$comment', '$image')";
-if (mysqli_query($conn, $sql)) {
-  echo "Comment submitted successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-?>
